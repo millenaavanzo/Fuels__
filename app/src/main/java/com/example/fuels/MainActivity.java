dddce543ceefb8bb235af985e9121b972d107a2d
millenaavanzo <millenaavanzo@gmail.com>
2019-03-21 12:17:16 -0300

    01

--- app/src/main/java/com/example/fuels/MainActivity.java
+++ app/src/main/java/com/example/fuels/MainActivity.java
@@ -6,13 +6,19 @@ import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.SeekBar;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
+    private NumberFormat n = NumberFormat.getCurrencyInstance();
     private SeekBar gasolinaSeekBar;
     private SeekBar etanolSeekBar;
     private TextInputLayout choice;
     private EditText choiceEditText;
     private ImageView image;
+    private TextView valueGasolina;
+    private TextView valueEthanol;
     private int gasolina;
     private int etanol;
     @Override
@@ -23,10 +29,14 @@ public class MainActivity extends AppCompatActivity {
         gasolinaSeekBar = findViewById(R.id.gasolineSeekBar);
         etanolSeekBar = findViewById(R.id.etanolSeekBar);
         choiceEditText = findViewById(R.id.choiceEditText);
+        valueEthanol = findViewById(R.id.valueEthanol);
+        valueGasolina = findViewById(R.id.valueGasolina);
         image = findViewById(R.id.image);
         gasolina=gasolinaSeekBar.getProgress();
         etanol=etanolSeekBar.getProgress();
         calculation(gasolina,etanol);
+        valueGasolina.setText(n.format(gasolina));
+        valueEthanol.setText(n.format(etanol));
         gasolinaSeekBar.setOnSeekBarChangeListener(new gasolinaSeekWatcher());
         etanolSeekBar.setOnSeekBarChangeListener(new etanolSeekWatcher());
     }
@@ -35,6 +45,8 @@ public class MainActivity extends AppCompatActivity {
         public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
             gasolina = progress;
             calculation(progress,etanol);
+
+            valueGasolina.setText(n.format(progress));
         }
 
         @Override
@@ -52,6 +64,8 @@ public class MainActivity extends AppCompatActivity {
         public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
             etanol = progress;
             calculation(gasolina,progress);
+
+            valueEthanol.setText(n.format(progress));
         }
 
         @Override
